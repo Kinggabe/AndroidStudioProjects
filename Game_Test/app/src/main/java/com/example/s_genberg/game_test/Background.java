42649f951b8e05f2a2ede3126bc7775b1b45102d
Ben Biggers <bbbiggers@gmail.com>
2015-05-28 08:28:37 -0700

    fixed

--- Game_Test/app/src/main/java/com/example/s_genberg/game_test/Background.java
+++ Game_Test/app/src/main/java/com/example/s_genberg/game_test/Background.java
@@ -4,35 +4,31 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
 public class Background {
-    private Bitmap spritesheet;
+
     private Bitmap image;
     private int x, y, dx;
-    private Animation animation = new Animation();
-    private long startTime;
 
-    public Background(Bitmap res, int w, int h, int numFrames) {
+    public Background(Bitmap res)
+    {
         image = res;
-        dx = GamePanel.MOVESPEED;
-        Bitmap[] image = new Bitmap[numFrames];
-        spritesheet = res;
-
-        for(int i = 0; i < image.length; i++) {
-            image[i] = Bitmap.createBitmap(spritesheet, i*w, 0, w, h);
-        }
-        startTime = System.nanoTime();
     }
-    public void update() {
-        animation.update();
-    x += dx;
-        if(x <-GamePanel.WIDTH) {
-            x = 0;
+    public void update()
+    {
+        x+=dx;
+        if(x<-GamePanel.WIDTH){
+            x=0;
         }
     }
-    public void draw(Canvas canvas) {
-    canvas.drawBitmap(image, x, y, null);
-        if(x< 0) {
+    public void draw(Canvas canvas)
+    {
+        canvas.drawBitmap(image, x, y,null);
+        if(x<0)
+        {
             canvas.drawBitmap(image, x+GamePanel.WIDTH, y, null);
         }
     }
-
+    public void setVector(int dx)
+    {
+        this.dx = dx;
+    }
 }
